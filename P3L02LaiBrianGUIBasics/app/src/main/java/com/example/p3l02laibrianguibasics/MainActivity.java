c9b65f8a230d8c4c7ccac15246300d7066fde903
BLCRAFT <brianslai@gmail.com>
2022-02-14 11:01:37 -0500

    upload

--- P3L02LaiBrianGUIBasics/app/src/main/java/com/example/p3l02laibrianguibasics/MainActivity.java
+++ P3L02LaiBrianGUIBasics/app/src/main/java/com/example/p3l02laibrianguibasics/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.p3l02laibrianguibasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button nameButton;
+    TextView nameDisplay;
+    EditText nameInput;
+    String[] colors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameButton = findViewById(R.id.name_button);
+        nameDisplay = findViewById(R.id.element_text);
+
+        colors = getResources().getStringArray(R.array.colors);
     }
 }
