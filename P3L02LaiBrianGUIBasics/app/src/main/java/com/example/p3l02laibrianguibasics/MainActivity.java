8e6b9ee43674363c006fa815a2041c64970e2790
BLCRAFT <brianslai@gmail.com>
2022-02-15 13:40:23 -0500

    finish l2

--- P3L02LaiBrianGUIBasics/app/src/main/java/com/example/p3l02laibrianguibasics/MainActivity.java
+++ P3L02LaiBrianGUIBasics/app/src/main/java/com/example/p3l02laibrianguibasics/MainActivity.java
@@ -9,10 +9,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button nameButton;
-    TextView nameDisplay;
+    Button nameButton, colorButton;
+    TextView nameDisplay, colorDisplay;
     EditText nameInput;
     String[] colors;
+    int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +23,31 @@ public class MainActivity extends AppCompatActivity {
         nameButton = findViewById(R.id.name_button);
         nameDisplay = findViewById(R.id.element_text);
         nameInput = findViewById(R.id.text_input);
+        colorButton = findViewById(R.id.color_button);
+        colorDisplay = findViewById(R.id.color_display);
 
         colors = getResources().getStringArray(R.array.colors);
+        colorDisplay.setText(colors[index]);
     }
 
     public void showName(View view) {
         nameDisplay.setText("Hello "+nameInput.getText());
+    }
+
+    public void changeColor(View view) {
+        index = (index+1)%colors.length;
+        colorDisplay.setText(colors[index]);
+        if (index==0)
+            view.setBackgroundColor(getResources().getColor(R.color.red));
+        else if (index==1)
+            view.setBackgroundColor(getResources().getColor(R.color.orange));
+        else if (index==2)
+            view.setBackgroundColor(getResources().getColor(R.color.yellow));
+        else if (index==3)
+            view.setBackgroundColor(getResources().getColor(R.color.green));
+        else if (index==4)
+            view.setBackgroundColor(getResources().getColor(R.color.blue));
+        else
+            view.setBackgroundColor(getResources().getColor(R.color.violet));
     }
 }
