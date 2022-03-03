94737e08337b4b1772d2fa6f2af6cc389f657eb0
BLCRAFT <brianslai@gmail.com>
2022-03-03 12:10:55 -0500

    upload

--- P3L04LaiBrianLayouts/app/src/main/java/com/example/p3l04laibrianlayouts/MainActivity.java
+++ P3L04LaiBrianLayouts/app/src/main/java/com/example/p3l04laibrianlayouts/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.p3l04laibrianlayouts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
 
+    int[] ids;
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout5);
+        setContentView(R.layout.layout1);
+        ids = new int[]{R.layout.layout1,R.layout.layout2,R.layout.layout3,R.layout.layout4,R.layout.layout5,R.layout.layout6,R.layout.layout7};
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction()==MotionEvent.ACTION_DOWN)
+        {
+            i = (++i)%ids.length;
+            setContentView(ids[i]);
+        }
+        return true;
     }
 }
