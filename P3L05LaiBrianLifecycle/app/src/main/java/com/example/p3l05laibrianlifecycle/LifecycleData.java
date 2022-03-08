bc1eff0c985ab83a7c81645e8b97f807235cb2cd
BLCRAFT <brianslai@gmail.com>
2022-03-08 14:13:37 -0500

    upload

---
+++ P3L05LaiBrianLifecycle/app/src/main/java/com/example/p3l05laibrianlifecycle/LifecycleData.java
@@ -0,0 +1,38 @@
+package com.example.p3l05laibrianlifecycle;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class LifecycleData {
+    int onCreate = 0;
+    int onStart = 0;
+    int onResume = 0;
+    int onPause = 0;
+    int onStop = 0;
+    int onRestart = 0;
+    int onDestroy = 0;
+    String duration;
+    public String toString()
+    {
+        return duration + "\n"+
+            "onCreate \t"+onCreate+"\n" +
+            "onStart \t"+onStart+"\n" +
+            "onResume \t"+onResume+"\n" +
+            "onPause \t"+onPause+"\n" +
+            "onStop \t"+onStop+"\n" +
+            "onRestart \t"+onRestart+"\n" +
+            "onDestroy \t"+onDestroy+"\n";
+    }
+
+    String toJson()
+    {
+        Gson gson = new Gson();
+        return gson.toJson(this, LifecycleData.class);
+    }
+
+    static LifecycleData parseJSON(String fromSharedPreferences)
+    {
+        Gson gson = new GsonBuilder().create();
+        return gson.fromJson(fromSharedPreferences, LifecycleData.class);
+    }
+}
--- P3L05LaiBrianLifecycle/app/src/main/java/com/example/p3l05laibrianlifecycle/MainActivity.java
+++ P3L05LaiBrianLifecycle/app/src/main/java/com/example/p3l05laibrianlifecycle/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.p3l05laibrianlifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG = "com.example.p3l05laibrianlifecycle.sharedpreferences"
+    LifecycleData currentRun, lifeTime;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    TextView currentRunTV, lifeTimeTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editor = sharedPreferences.edit();
+        currentRun = new LifecycleData();
+        currentRun.duration = "Current Run";
+
+        String lifecycleDataAsString = sharedPreferences.getString("lifetime", "");
+        if (lifecycleDataAsString.equals(""))
+        {
+
+        }
     }
 }
