06b91c73853fd19ee7a4f0053e7c92e0d62320fd
BLCRAFT <brianslai@gmail.com>
2022-03-24 14:15:20 -0400

    upload

---
+++ P3L08LaiBrianDrawing/app/src/main/java/com/example/p3l08laibriandrawing/DrawView.java
@@ -0,0 +1,24 @@
+package com.example.p3l08laibriandrawing;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class DrawView extends View {
+    public DrawView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint p = new Paint();
+        p.setColor(Color.CYAN);
+        canvas.drawCircle(100,100,10, p);
+    }
+}
