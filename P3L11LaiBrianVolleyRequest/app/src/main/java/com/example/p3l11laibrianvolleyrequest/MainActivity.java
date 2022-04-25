2904014a54ea38c89218ba08c35e9b7d568360a1
BLCRAFT <brianslai@gmail.com>
2022-04-25 11:38:07 -0400

    upload

---
+++ P3L11LaiBrianVolleyRequest/app/src/main/java/com/example/p3l11laibrianvolleyrequest/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.p3l11laibrianvolleyrequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    RequestQueue queue;
+    String URL = "https://mw-demo.sites.tjhsst.edu/data";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview);
+        queue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                
+            }
+        })
+    }
+}
