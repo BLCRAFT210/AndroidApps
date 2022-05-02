d41a446944cf3cd655c016a1cb96877597bfc34d
BLCRAFT <brianslai@gmail.com>
2022-05-02 13:48:41 -0400

    upload

--- P3L11LaiBrianVolleyRequest/app/src/main/java/com/example/p3l11laibrianvolleyrequest/MainActivity.java
+++ P3L11LaiBrianVolleyRequest/app/src/main/java/com/example/p3l11laibrianvolleyrequest/MainActivity.java
@@ -8,8 +8,13 @@ import android.widget.TextView;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
@@ -25,8 +30,26 @@ public class MainActivity extends AppCompatActivity {
         StringRequest request = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                
+                try {
+                    JSONObject object = new JSONObject("{\"data\":"+response+"}");
+                    JSONArray array = object.getJSONArray("data");
+                    String toPrint = "";
+                    for (int id = 0;id<=9;id++)
+                    {
+                        JSONObject currentData = array.getJSONObject(id);
+                        toPrint += "id: "+currentData.getString("id")+" | category: "+currentData.getString("category")+" / "+currentData.getString("increment")+"-"+currentData.getString("decrement")+"\n";
+                    }
+                    textView.setText(toPrint);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-        })
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Volley error"+error.toString());
+            }
+        });
+        queue.add(request);
     }
 }
